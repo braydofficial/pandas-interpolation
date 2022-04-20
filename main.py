6be73628c54cf571bcffb9a1d7d46d67c375176f
Brayd <byronfroehlich@pm.me>
2022-04-20 12:56:47 +0200

    Change code to make use of isnull function

--- main.py
+++ main.py
@@ -2,22 +2,14 @@
 import pandas as pd
 
 # Define the variable df which stands for DataFrame and create a DataFrame from the CSV-File. Also setting the seperator to a semicolon
-# because it would otherwise not work. By default this function uses a comma as the seperator.
-df = pd.read_csv('./data.csv', sep=';')
+# because it would otherwise not work. By default this function uses a comma as the seperator. Because the CSV-file has a comment
+# in the first line, we give an argument named comment and define which character specifies the comment in the CSV-file
+df = pd.read_csv('./data.csv', sep=';', comment='#')
 
-# Accessing the column called Jan from the DataFrame created in the last step and make a linear interpolation on it.
-# The argument axis defines in which direction the interpolation should be made. axis=0 means it will interpolate the column.
-# If the axis would be set to 1 it would try to interpolate the row instead of the column, which isn't what we want to do here.
-df['Jan'] = df['Jan'].interpolate(method='linear', axis=0)
-
-# Print out the result from the interpolation.
-print('\n\nJan: \n')
-print(df['Jan'])
-
-df['Feb'] = df['Feb'].interpolate(method='linear', axis=0)
-print('\n\nFeb: \n')
-print(df['Feb'])
-
-df['Mar'] = df['Mar'].interpolate(method='linear', axis=0)
-print('\n\nMar: \n')
-print(df['Mar'])
+# We create a bool_series on the index column of the CSV-File called year
+bool_series = pd.isnull(df['Year'])
+# Now we asign the bool_series to the DataFrame we created
+df[bool_series]
+# And with this command we create a linear interpolation based on the data we created and print it out. It will now print out the data for the missing
+# fields.
+print(df.interpolate(method='linear', axis=0))
